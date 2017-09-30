2749d23528dac835eb552fe91c9a83db7e4ac944
Timothy DeVries <devritim@mail.gvsu.edu>
2017-09-30 02:18:03 -0400

    Reads file and reverses the text, but does not write to file.

--- file_utils.c
+++ file_utils.c
@@ -1,5 +1,26 @@
 #include "file_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
+int read_file( char* filename, char **buffer ) {
+	struct stat st;
+	stat(filename, &st);
+	int size = st.st_size;
+	FILE *inFile = fopen( filename, "r" );
+	*buffer = (char *) malloc( size*sizeof(char) );
+	fgets( *buffer, size, inFile );
+	fclose( inFile );
+	return 0;
+}
 
-
+int write_file( char* filename, char *buffer, int size ) {
+	FILE *outFile = fopen( filename, "w" );
+	for ( int i = 0; i < size; i++ ) {
+		fputc(buffer[i], outFile);
+	}
+	//fputs( buffer, outFile );
+	fclose( outFile );
+	return 0;
+}
 
--- reverse.c
+++ reverse.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
-#include <.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include "file_utils.h"
 
 int main(int argc, char** argv) {
-	int bytes;
-	char *buffer;
-	
+	char *in;
+	char *out;
+	printf("File reversal begun.");
+	if ( argc != 3 ) {
+		printf("Invalid arguments.\n");
+		return -1;
+	}
+	if ( access( argv[1], F_OK ) == -1 ) {
+		printf("Input file does not exist.\nCheck spelling and try again.\n");
+		return -1;
+	}
+	printf("%i \n", argc);
+	for ( int i = 0; i < argc; i++ ) {
+		printf("%s \n", argv[i]);
+	}
+	struct stat st;
+	stat(argv[1], &st);
+	int size = st.st_size;
+	printf("Size: %i\n", size);
+	read_file( argv[1], &in );
+	out = (char *) malloc( size*sizeof(char) );
+	for ( int i = 0; i < size; i++ ) {
+		out[i] = in[size - ( i + 1 )];
+	}
+	write_file( argv[2], out, size);
+	printf("File reversal completed.");
 }
