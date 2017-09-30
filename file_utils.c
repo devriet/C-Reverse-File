b99bdcb220662d52f6f9e03401e211c71e6e87a9
Timothy DeVries <devritim@mail.gvsu.edu>
2017-09-30 15:33:23 -0400

    Project complete and tested.

--- file_utils.c
+++ file_utils.c
@@ -9,17 +9,20 @@ int read_file( char* filename, char **buffer ) {
 	int size = st.st_size;
 	FILE *inFile = fopen( filename, "r" );
 	*buffer = (char *) malloc( size*sizeof(char) );
-	fgets( *buffer, size, inFile );
+	/*for ( int i = 0; i < size; i++ ) {
+		*buffer[i] = (char) fgetc( inFile );
+	}*/
+	fread( *buffer, sizeof(char), size, inFile );
 	fclose( inFile );
 	return 0;
 }
 
 int write_file( char* filename, char *buffer, int size ) {
 	FILE *outFile = fopen( filename, "w" );
-	for ( int i = 0; i < size; i++ ) {
+	/*for ( int i = 0; i < size; i++ ) {
 		fputc(buffer[i], outFile);
-	}
-	//fputs( buffer, outFile );
+	}*/
+	fputs( buffer, outFile );
 	fclose( outFile );
 	return 0;
 }
--- reverse.c
+++ reverse.c
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
 	char *in;
 	char *out;
-	printf("File reversal begun.");
+	printf("File reversal begun.\n");
 	if ( argc != 3 ) {
 		printf("Invalid arguments.\n");
 		return -1;
@@ -16,19 +16,19 @@ int main(int argc, char** argv) {
 		printf("Input file does not exist.\nCheck spelling and try again.\n");
 		return -1;
 	}
-	printf("%i \n", argc);
+	/*printf("%i \n", argc);
 	for ( int i = 0; i < argc; i++ ) {
 		printf("%s \n", argv[i]);
-	}
+	}*/
 	struct stat st;
 	stat(argv[1], &st);
 	int size = st.st_size;
-	printf("Size: %i\n", size);
+	//printf("Size: %i\n", size);
 	read_file( argv[1], &in );
 	out = (char *) malloc( size*sizeof(char) );
 	for ( int i = 0; i < size; i++ ) {
 		out[i] = in[size - ( i + 1 )];
 	}
 	write_file( argv[2], out, size);
-	printf("File reversal completed.");
+	printf("File reversal completed.\n");
 }
